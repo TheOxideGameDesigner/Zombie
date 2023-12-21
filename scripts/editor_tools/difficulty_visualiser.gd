2fa41b071b428276a56f25fe53f655177efd6345
TheOxideGamer <152723687+TheOxideGameDesigner@users.noreply.github.com>
2023-12-21 14:26:41 +0200

    actually fixed particle bug fr fr

--- scripts/editor_tools/difficulty_visualiser.gd
+++ scripts/editor_tools/difficulty_visualiser.gd
@@ -2,12 +2,11 @@
 extends Node
 
 var target_diff = 2
-@onready var diff = get_parent().min_dif
 @export var radius = 0.5
 @export var height = 2.0
 
 var mesh
-const COLORS = [Color(0.5, 1, 0, 0.2), Color(0, 0.7, 0, 0.3), Color(1.0, 1.0, 0, 0.5), Color(1.0, 0.3, 0.0, 0.3), Color(0.3, 0.0, 0.3, 0.4)]
+const COLORS = [Color(1, 1, 1, 0.2), Color(0, 0.7, 0, 0.3), Color(1.0, 1.0, 0, 0.5), Color(1.0, 0.3, 0.0, 0.3), Color(0.3, 0.0, 0.3, 0.4)]
 
 var prev_pressed_plus = false
 var prev_pressed_minus = false
@@ -15,6 +14,7 @@ var prev_pressed_minus = false
 func _ready():
 	if Engine.is_editor_hint():
 		mesh = MeshInstance3D.new()
+		mesh.add_to_group("diff_vis")
 		mesh.mesh = CylinderMesh.new()
 		mesh.mesh.top_radius = radius
 		mesh.mesh.bottom_radius = radius
@@ -23,21 +23,21 @@ func _ready():
 		mesh.material_override = StandardMaterial3D.new()
 		mesh.material_override.shading_mode = StandardMaterial3D.SHADING_MODE_UNSHADED
 		mesh.material_override.transparency = StandardMaterial3D.TRANSPARENCY_ALPHA
-		mesh.material_override.albedo_color = COLORS[diff]
 		get_parent().get_node("mesh").add_child(mesh)
 	else:
 		queue_free()
 
 func _process(delta):
+	mesh.material_override.albedo_color = COLORS[get_parent().min_dif]
 	if not Input.is_key_pressed(KEY_SHIFT):
 		return
 	var pressed_minus = Input.is_key_pressed(KEY_O)
 	var pressed_plus = Input.is_key_pressed(KEY_P)
 	if pressed_minus and not prev_pressed_minus:
 		target_diff = max(0, target_diff - 1)
-		mesh.get_parent().get_parent().visible = diff <= target_diff
+		mesh.get_parent().get_parent().visible = get_parent().min_dif <= target_diff
 	if pressed_plus and not prev_pressed_plus:
 		target_diff = min(4, target_diff + 1)
-		mesh.get_parent().get_parent().visible = diff <= target_diff
+		mesh.get_parent().get_parent().visible = get_parent().min_dif <= target_diff
 	prev_pressed_minus = pressed_minus
 	prev_pressed_plus = pressed_plus
--- scripts/environment/blood_particles.gd
+++ scripts/environment/blood_particles.gd
@@ -8,9 +8,11 @@ var spread = 27.75
 
 
 func _ready():
-	process_material = process_material.duplicate()
+	process_material = ParticleProcessMaterial.new()
 	emitting = true
 	process_material.direction = dir
+	process_material.color_ramp = GradientTexture1D.new()
+	process_material.color_ramp.gradient = Gradient.new()
 	process_material.color_ramp.gradient.colors = [Color(color.r, color.g, color.b, 1), Color(color.r, color.g, color.b, 0)]
 	process_material.initial_velocity_min = speed
 	process_material.initial_velocity_max = 2 * speed
