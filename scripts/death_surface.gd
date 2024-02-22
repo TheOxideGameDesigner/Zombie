8c43591fcc957e547902265f2fcb12e15e50808f
TheOxideGamer <152723687+TheOxideGameDesigner@users.noreply.github.com>
2024-02-23 00:37:27 +0200

    fixed a crash again

--- scripts/death_surface.gd
+++ scripts/death_surface.gd
@@ -7,5 +7,7 @@ func _ready():
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-	if body.is_in_group("player") or body.is_in_group("lightweight"):
+	if body.is_in_group("player"):
 		body.pain("You stepped on a dangerous surface", 9001)
+	elif body.is_in_group("lightweight"):
+		body.pain(9001)
