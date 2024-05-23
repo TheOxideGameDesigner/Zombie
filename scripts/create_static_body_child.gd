59009974338042954328b281d43c8ba4c1340b9c
TheOxideGamer <152723687+TheOxideGameDesigner@users.noreply.github.com>
2024-05-23 21:46:20 +0300

    polished c3

--- scripts/create_static_body_child.gd
+++ scripts/create_static_body_child.gd
@@ -1,6 +1,7 @@
 extends MeshInstance3D
 
 @export var hide_mesh = true
+@export var layer = 1
 
 func _ready():
 	for i in get_children():
@@ -8,3 +9,5 @@ func _ready():
 	if hide_mesh:
 		visible = 0
 	create_trimesh_collision()
+	var body : StaticBody3D = get_children()[0]
+	body.collision_layer = layer
